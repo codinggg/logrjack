b7ffe4e802a1678c4c0c58b768dd80ae44a66a19
Jud White <jud_white@dell.com>
2017-01-15 08:45:29 -0600

    support github.com/pkg/errors stack traces

--- entry.go
+++ entry.go
@@ -2,7 +2,7 @@ package logrjack
 
 import (
 	"fmt"
-	"runtime"
+
 	"strings"
 
 	"github.com/Sirupsen/logrus"
@@ -26,11 +26,6 @@ type Entry interface {
 
 	// AddFields adds a map of fields to the Entry.
 	AddFields(fields map[string]interface{})
-
-	// AddCallstack adds the current callstack to the Entry using the key "callstack".
-	//
-	// Excludes runtime/proc.go, http/server.go, and files ending in .s from the callstack.
-	AddCallstack()
 
 	// Info logs the Entry with a status of "info".
 	Info(args ...interface{})
@@ -68,18 +63,6 @@ func NewEntry() Entry {
 	return e
 }
 
-// Callstack creates a new log Entry with the callstack. If 'err' is not nil,
-// an entry "err" is added with the specified error.
-func Callstack(err error) Entry {
-	e := &entry{}
-	e.entry = logrus.NewEntry(std)
-	e.addCallstack(3)
-	if err != nil {
-		e.AddError(err)
-	}
-	return e
-}
-
 // String returns the string representation from the reader and
 // ultimately the formatter.
 func (e entry) String() string {
@@ -93,6 +76,15 @@ func (e entry) String() string {
 // AddError adds a field "err" with the specified error.
 func (e *entry) AddError(err error) {
 	e.AddField("err", err)
+
+	var cs []string
+	for _, frame := range StackTrace(err) {
+		cs = append(cs, fmt.Sprintf("%s:%s:%d", frame.File(), frame.Func(), frame.Line()))
+	}
+
+	if len(cs) > 0 {
+		e.AddField("stacktrace", strings.Join(cs, ", "))
+	}
 }
 
 // AddField adds a single field to the Entry.
@@ -107,30 +99,6 @@ func (e *entry) AddFields(fields map[string]interface{}) {
 		logrusFields[k] = v
 	}
 	e.entry = e.entry.WithFields(logrusFields)
-}
-
-// AddCallstack adds the current callstack to the Entry using the key "callstack".
-//
-// Excludes runtime/proc.go, http/server.go, and files ending in .s from the callstack.
-func (e *entry) AddCallstack() {
-	e.addCallstack(3)
-}
-
-func (e *entry) addCallstack(skipFrames int) {
-	var cs []string
-	for i := 0; ; i++ {
-		file, line := getCaller(skipFrames + i)
-		if file == "???" {
-			break
-		}
-		if strings.HasSuffix(file, ".s") ||
-			strings.HasPrefix(file, "http/server.go") ||
-			strings.HasPrefix(file, "runtime/proc.go") {
-			continue
-		}
-		cs = append(cs, fmt.Sprintf("%s:%d", file, line))
-	}
-	e.AddField("callstack", strings.Join(cs, ", "))
 }
 
 // Info logs the Entry with a status of "info".
@@ -171,27 +139,4 @@ func (e *entry) Fatal(args ...interface{}) {
 // Fatalf logs the Entry with a status of "fatal" and calls os.Exit(1).
 func (e *entry) Fatalf(format string, args ...interface{}) {
 	e.entry.Fatalf(format, args...)
-}
-
-func getCaller(skip int) (file string, line int) {
-	var ok bool
-	_, file, line, ok = runtime.Caller(skip)
-	if !ok {
-		file = "???"
-		line = 0
-	} else {
-		short := file
-		count := 0
-		for i := len(file) - 1; i > 0; i-- {
-			if file[i] == '/' {
-				count++
-				if count == 2 {
-					short = file[i+1:]
-					break
-				}
-			}
-		}
-		file = short
-	}
-	return
 }
--- entry_test.go
+++ entry_test.go
@@ -1,20 +1,24 @@
 package logrjack
 
 import (
+	"io"
 	"strings"
 	"testing"
+
+	"github.com/pkg/errors"
 )
 
 func TestAddCallstack(t *testing.T) {
 	// arrange
+	want := `stacktrace="github.com/judwhite/logrjack/entry_test.go:TestAddCallstack:17"`
 	e := NewEntry()
 
 	// act
-	e.AddCallstack()
+	e.AddError(errors.WithStack(io.EOF))
 
 	// assert
 	actual := e.String()
-	if !strings.Contains(actual, "callstack=\"logrjack/entry_test.go:13, ") {
-		t.Fatalf("could not find expected callstack string: %s", actual)
+	if !strings.Contains(actual, want) {
+		t.Fatalf("could not find expected callstack string: '%s'\ngot:\n\t%s", want, actual)
 	}
 }
